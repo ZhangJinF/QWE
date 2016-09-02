c4aca8fcae2eeed2d4fbb8bc48f1ada4a2647fb5
ZhangJinF <3014227662@qq.com>
2016-09-01 23:11:49 -0500

    test

---
+++ cccc.java
@@ -0,0 +1,13 @@
+package nnnn;
+
+public class cccc {
+public static void main(String[] args) {
+double a=2.5,b=2.0;
+int c=3000,d=4500;
+double m=a*c;
+double n=b*d;
+double v=m+n;
+int t=(int)v;
+System.out.println(t);
+}
+}
